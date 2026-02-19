d8abff4ab2bf584480b08fb470a84e0bde49bbe4
pelayowindel <windel.pelayo@dynata.com>
2026-02-19 19:44:45 +0800

    feat: add dotenv support and configuration for environment variables

---
+++ app.config.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'hitch',
+  slug: 'hitch',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'hitch',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/images/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: ['expo-router'],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  },
+};
+
+export default config;
